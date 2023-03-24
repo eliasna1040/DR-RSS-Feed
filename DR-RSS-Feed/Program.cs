9520232a495d73c50e8ea97d82ed68ba60d8008b
c98101ena <elias.andersen@hessel.dk>
2023-03-24 08:27:54 +0100

    Add project files.

---
+++ DR-RSS-Feed/Program.cs
@@ -0,0 +1,13 @@
+﻿using DR_RSS_Feed;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+XDocument.Load("https://www.dr.dk/nyheder/service/feeds/senestenyt").Root.Element("channel").Elements("item").Select(x => new { Title = x.Element("title").Value, Link = x.Element("link").Value, PubDate = x.Element("pubDate").Value }).ToList().ForEach(x => Console.WriteLine($"{x.Title}\n{x.Link}\n{x.PubDate}\n"));
+
+Console.WriteLine();
+
+XmlSerializer serializer = new(typeof(Rss));
+Rss? rss = (Rss?)serializer.Deserialize(XmlReader.Create("https://www.dr.dk/nyheder/service/feeds/senestenyt"));
+
+rss?.Channel.Item.ForEach(x => Console.WriteLine($"{x.Title}\n{x.Link}\n{x.PubDate}\n"));
---
+++ DR-RSS-Feed/XmlClasses.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DR_RSS_Feed
+{
+    [XmlRoot(ElementName = "link")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "guid")]
+    public class Guid
+    {
+        [XmlAttribute(AttributeName = "isPermaLink")]
+        public string IsPermaLink { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; }
+        [XmlElement(ElementName = "guid")]
+        public Guid Guid { get; set; }
+        [XmlElement(ElementName = "pubDate")]
+        public string PubDate { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "generator")]
+        public string Generator { get; set; }
+        [XmlElement(ElementName = "lastBuildDate")]
+        public string LastBuildDate { get; set; }
+        [XmlElement(ElementName = "language")]
+        public string Language { get; set; }
+        [XmlElement(ElementName = "docs")]
+        public string Docs { get; set; }
+        [XmlElement(ElementName = "link")]
+        public Link Link { get; set; }
+        [XmlElement(ElementName = "item")]
+        public List<Item> Item { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class Rss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+    }
+}
